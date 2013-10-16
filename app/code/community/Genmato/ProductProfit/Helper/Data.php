9326bd8b1b93eaf8b258eaed777c0209ec399e98
V. Kerkhoff <v.kerkhoff@genmato.com>
2013-10-16 10:13:08 +0200

    Initial setup

---
+++ app/code/community/Genmato/ProductProfit/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @category    Genmato
+ * @package     Genmato_ProductProfit
+ * @copyright   Copyright (c) 2013 Genmato BV (http://genmato.com)
+ */
+
+class Genmato_ProductProfit_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}

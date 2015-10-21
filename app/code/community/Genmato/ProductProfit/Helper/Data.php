c9a743a26b40658367cd162de4816f9db5cde46d
Vladimir Kerkhoff <v.kerkhoff@genmato.com>
2015-10-21 14:52:31 +0200

    Version: 15.293.1 Updated logging

--- app/code/community/Genmato/ProductProfit/Helper/Data.php
+++ app/code/community/Genmato/ProductProfit/Helper/Data.php
@@ -5,7 +5,8 @@
  * @package     Genmato_ProductProfit
  * @copyright   Copyright (c) 2013 Genmato BV (http://genmato.com)
  */
-class Genmato_ProductProfit_Helper_Data extends Mage_Core_Helper_Abstract
+class Genmato_ProductProfit_Helper_Data extends Genmato_Core_Helper_Data
 {
+    const DEBUG_PATH = 'genmato_productprofit/logging/enabled';
 
 }
